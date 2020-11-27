9485f001d167e0f4f82cc138d87af9443f045554
abhijitsahu57 <71395652+abhijitsahu57@users.noreply.github.com>
2020-11-27 13:19:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+fill("white")
+function draw() {
+  background("black");
+  textSize(20);
+  text("Score : "+score + "00",20,30);
+  text ( "500" , 20 ,530 );
+  text ( "1000", 100 ,530 );
+  text ( "100", 180 ,530 );
+  text ( "200", 260 ,530 );
+  text ( "300", 340 ,530 );
+  text ( "600", 420 ,530 );
+  text ( "900", 500 ,530 );
+  text ( "10", 590 ,530 );
+  text ( "100",660 ,530 );
+  text ( "20", 740 ,530 );
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%30===0){
+     particles.push(new Particle(random(width/2-300, width/2+300), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
